5460706dcc9e15723fe07b24b89857d6396b3177
Peter Čagala <petercagala@gmail.com>
2019-12-22 13:22:07 +0100

    [0-1] Default commit

---
+++ src/main/java/sk/cagalpte/learn2code/javascript/coremodul/CoreModulApplication.java
@@ -0,0 +1,13 @@
+package sk.cagalpte.learn2code.javascript.coremodul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreModulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreModulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/cagalpte/learn2code/javascript/coremodul/CoreModulApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.cagalpte.learn2code.javascript.coremodul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreModulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
