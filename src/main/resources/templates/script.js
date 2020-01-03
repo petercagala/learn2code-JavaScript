f08cc31c685b46cfee1f449c67542e354074768f
Peter Čagala <petercagala@gmail.com>
2020-01-03 09:44:09 +0100

    [6-2] Zadanie 2, vypis vysledok

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -9,9 +9,52 @@ myButton.addEventListener('click',function clicked(event) {
     addListItemMessage("browser","klikol si na button")
 });
 
-document.body.addEventListener('keyup', function keyboarded(event) {
+document.getElementById("computeId").addEventListener("click", function computeFunction(event) {
+    // var numberOfItems = 5;
+    var numberOfItems = document.getElementById("numberOfItems").value
+    var itemPrice = document.getElementById("itemPrice").value;
+    let computeParagraph = document.getElementById("computeParagraph");
+    computeParagraph.innerHTML = "";
+
+    var resultPrice = computePrice(numberOfItems, itemPrice);
+
+    console.log(resultPrice);
+    computeParagraph.append("Kupil si "+ numberOfItems + " predmetov za " + resultPrice + " eur dokopy!!!");
+})
+
+document.body.addEventListener('keypress', function keyboarded(event) {
+// document.body.addEventListener('keyup', function keyboarded(event) {
     console.log(event);
-    addListItemMessage("user", "stlacil si tlacitko na klavesnici")
+
+    var oldLeft = getComputedStyle(document.body).left,
+        newLeft;
+    // Urci vzdy, v akej desiatkovej sustave prave pracujes, defaultna je 10 tkova
+    oldLeft = parseInt(oldLeft, 10);
+
+    var oldUp = getComputedStyle(document.body).margin,
+        newUp;
+    oldUp = parseInt(oldUp, 10);
+
+    // v starsich prehliadacoch sa pouziva event.keyCode
+    if (event.key == "a") {
+        addListItemMessage("user", event.key + " and go LEFT");
+        newLeft = oldLeft - 10;
+    } else if (event.key == "l"){
+        addListItemMessage("user", event.key + " and go RIGHT");
+        newLeft = oldLeft + 10;
+    } else if (event.key = "y") {
+        // addListItemMessage("user", event.key + " and go UP");
+        newUp = oldUp - 10;
+    } else if (event.key = "b") {
+        // addListItemMessage("user", event.key + " and go DOWN");
+        newUp = oldUp + 10;
+    }
+    else {
+        addListItemMessage("user", event.key);
+    }
+    document.body.style.left = newLeft + "px";
+    document.body.style.margin = newUp + " px";
+
 });
 
 window.addEventListener('beforeunload', function (event) {
@@ -20,6 +63,10 @@ window.addEventListener('beforeunload', function (event) {
     event.returnValue = 'Do you want to close this window?';
     console.log(event);
 })
+
+function computePrice(numberOfItems, itemPrice) {
+    return numberOfItems * itemPrice;
+}
 
 function addListItemMessage(idElement, message) {
     let userList = document.getElementById(idElement),
