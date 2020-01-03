8347ed6351d0562fe97a3c753ef97b4354920322
Peter Čagala <petercagala@gmail.com>
2020-01-03 07:17:03 +0100

    [6-1] addEventListener on window: window.addEventListener

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -5,16 +5,23 @@ newItem1.textContent = 'page loaded';
 browserList.appendChild(newItem1);
 
 let myButton = document.getElementById("myButtonId");
-myButton.addEventListener('click',function clicked() {
+myButton.addEventListener('click',function clicked(event) {
     addListItemMessage("browser","klikol si na button")
 });
 
-document.body.addEventListener('keyup', function keyboarded() {
+document.body.addEventListener('keyup', function keyboarded(event) {
+    console.log(event);
     addListItemMessage("user", "stlacil si tlacitko na klavesnici")
 });
 
-function addListItemMessage(idElement, message) {
+window.addEventListener('beforeunload', function (event) {
+    addListItemMessage('browser', "don't try to close me");
+
+    event.returnValue = 'Do you want to close this window?';
     console.log(event);
+})
+
+function addListItemMessage(idElement, message) {
     let userList = document.getElementById(idElement),
         userListItem = document.createElement('li');
 
