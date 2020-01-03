6a91a4bd32a2c2af2df944845c2cd2944f572266
Peter Čagala <petercagala@gmail.com>
2020-01-03 10:57:20 +0100

    [11-0] Zadanie 3

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -9,6 +9,15 @@ myButton.addEventListener('click',function clicked(event) {
     addListItemMessage("browser","klikol si na button")
 });
 
+document.getElementById("numberComparisonButtonId").addEventListener('click',function () {
+    let numberComparisonParagraph = document.getElementById("numberComparisonParagraphId");
+    numberComparisonParagraph.innerHTML = "";
+    // higherNumber(firstNumber, secondNumber)
+    var firstNumber = document.getElementById("firstNumber").value;
+    var secondNumber = document.getElementById("secondNumber").value;
+    numberComparisonParagraph.append("Najvacsie cislo je " + higherNumber(firstNumber, secondNumber));
+})
+
 document.getElementById("computeId").addEventListener("click", function computeFunction(event) {
     // var numberOfItems = 5;
     var numberOfItems = document.getElementById("numberOfItems").value
@@ -21,39 +30,45 @@ document.getElementById("computeId").addEventListener("click", function computeF
     console.log(resultPrice);
     computeParagraph.append("Kupil si "+ numberOfItems + " predmetov za " + resultPrice + " eur dokopy!!!");
 })
+var pressedKeys = {};
+document.body.addEventListener('keyup', function (event) {
+    pressedKeys[event.key] = false;
+})
 
 document.body.addEventListener('keypress', function keyboarded(event) {
 // document.body.addEventListener('keyup', function keyboarded(event) {
     console.log(event);
+
+    pressedKeys[event.key] = true;
 
     var oldLeft = getComputedStyle(document.body).left,
         newLeft;
     // Urci vzdy, v akej desiatkovej sustave prave pracujes, defaultna je 10 tkova
     oldLeft = parseInt(oldLeft, 10);
 
-    var oldUp = getComputedStyle(document.body).margin,
-        newUp;
-    oldUp = parseInt(oldUp, 10);
+    var oldTop = getComputedStyle(document.body).top,
+        newTop;
+    oldTop = parseInt(oldTop, 10);
 
     // v starsich prehliadacoch sa pouziva event.keyCode
-    if (event.key == "a") {
-        addListItemMessage("user", event.key + " and go LEFT");
+    if (pressedKeys['a']) {
         newLeft = oldLeft - 10;
-    } else if (event.key == "l"){
-        addListItemMessage("user", event.key + " and go RIGHT");
+    } else if (pressedKeys['l']){
         newLeft = oldLeft + 10;
-    } else if (event.key = "y") {
+    } else if (pressedKeys['y']) {
         // addListItemMessage("user", event.key + " and go UP");
-        newUp = oldUp - 10;
-    } else if (event.key = "b") {
+        newTop = oldTop - 10;
+    } else if (pressedKeys['b']) {
         // addListItemMessage("user", event.key + " and go DOWN");
-        newUp = oldUp + 10;
-    }
-    else {
+        newTop = oldTop + 10;
+    } else if (pressedKeys['y'] && pressedKeys['l']) {
+        newTop = oldTop - 10;
+        newLeft = oldLeft + 10;
+    } else {
         addListItemMessage("user", event.key);
     }
     document.body.style.left = newLeft + "px";
-    document.body.style.margin = newUp + " px";
+    document.body.style.top = newTop + "px";
 
 });
 
@@ -75,6 +90,13 @@ function addListItemMessage(idElement, message) {
     userListItem.textContent = message;
 
     userList.appendChild(userListItem);
+}
+
+function higherNumber(firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        return firstNumber
+    }
+    return secondNumber;
 }
 
 
