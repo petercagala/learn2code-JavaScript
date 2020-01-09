5fe1a9101f2f5dc817676287a425fab665f60306
Peter Čagala <petercagala@gmail.com>
2020-01-09 10:12:18 +0100

    [20-1] loading new key data to localstorage and getting data from localstorage

--- src/main/resources/templates/js/script2.js
+++ src/main/resources/templates/js/script2.js
@@ -8,6 +8,78 @@ var imagesElements = document.querySelectorAll('form img');
 // imagesElements = Array.from(imagesElements); toto je druha, jednoduchsia moznost
 imagesElements = Array.prototype.slice.call(imagesElements);
 
+
+imagesElements.forEach(function (imageElement) {
+
+    // load score from localstorage
+    updateDude(imageElement);
+
+    // increase score
+    imageElement.addEventListener('click', function () {
+        increaseScoreForDude(imageElement);
+    });
+
+    // Possible event types on image Element
+    let eventsArray = ['mouseover', 'mouseout'];
+    guyHurtsAndUnhurt(eventsArray, imageElement);
+});
+
+/**
+ * Increase score for concrete dude and save this new increased score to localstorage
+ * @param imageElement: concrete dude element, on who we are currently working
+ */
+function increaseScoreForDude(imageElement) {
+    // get key from score dude Vasho
+    console.log(imageElement.alt);
+    var dudeKey = getKeyFromDude(imageElement);
+
+    // get score from localstorage
+    var scoreAmount = getScore(imageElement);
+
+    // set newScore to localstorage
+    var newScoreAmount = scoreAmount + 1;
+    setScore(imageElement, newScoreAmount);
+
+    // UPDATE DOM element
+    updateDude(imageElement);
+}
+
+function updateDude(dude) {
+    var scoreElement = dude.nextElementSibling;
+    scoreElement.textContent = getScore(dude);
+}
+
+function getScore(dude) {
+    return +localStorage.getItem(getKeyFromDude(dude)) || 0;
+}
+
+function setScore(dude, score) {
+    localStorage.setItem(getKeyFromDude(dude), score);
+}
+
+/**
+ * Create key name for concrete dude by score + nameOfDude
+ * @param dude: concrete dude element
+ * @returns {string}: score + nameOfDude
+ */
+function getKeyFromDude(dude) {
+    return "score" + _.capitalize(dude.alt); ;
+}
+
+function desaturateClassRemoveOrAdd(selectedPerson) {
+    var otherImage = _.without(imagesElements, selectedPerson)[0];
+    otherImage.classList.toggle("desaturate");
+}
+
+function guyHurtsAndUnhurt(eventsArray, selectedPerson) {
+    eventsArray.forEach(function (eventItem) {
+        selectedPerson.addEventListener(eventItem, function () {
+            desaturateClassRemoveOrAdd(selectedPerson);
+        });
+    });
+}
+
+
 document.getElementsByTagName("h1")[0].addEventListener('click', function () {
     var scoreNumber = localStorage.getItem("scoreNumber") ;
     if (scoreNumber == null) {
@@ -23,35 +95,4 @@ document.getElementsByTagName("h1")[0].addEventListener('click', function () {
     if (scoreNumber > 7) {
         localStorage.removeItem("scoreNumber");
     }
-})
-
-imagesElements.forEach(function (imageElement) {
-    // increase score
-    imageElement.addEventListener('click', function () {
-        console.log(this);
-        var scoreElement = this.nextElementSibling;
-        var scoreAmount = Number(scoreElement.textContent);
-
-        scoreAmount = scoreAmount + 1;
-
-        console.log(scoreAmount);
-        scoreElement.textContent = scoreAmount;
-    });
-
-    // Possible event types on image Element
-    let eventsArray = ['mouseover', 'mouseout'];
-    guyHurtsAndUnhurts(eventsArray, imageElement, this);
 });
-
-function desaturateClassRemoveOrAdd(selectedPerson) {
-    var otherImage = _.without(imagesElements, selectedPerson)[0];
-    otherImage.classList.toggle("desaturate");
-}
-
-function guyHurtsAndUnhurts(eventsArray, imageElement, selectedPerson) {
-    eventsArray.forEach(function (eventItem) {
-        imageElement.addEventListener(eventItem, function () {
-            desaturateClassRemoveOrAdd(this);
-        });
-    });
-}
