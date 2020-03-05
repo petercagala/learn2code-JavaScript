2f11418eba9f8fd8bbbc590bfd86aad8ac1f5ae6
Peter Čagala <petercagala@gmail.com>
2020-03-05 13:55:36 +0100

    [5-3] button 2, osetrenie button 2

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -1,12 +1,19 @@
 var browserList = document.getElementById("browser"),
     newItem1 = document.createElement('li');
+    newItem2 = document.createElement('li');
 ;
-newItem1.textContent = 'page loaded';
+newItem1.textContent = 'page loaded 1';
+newItem2.textContent = 'page loaded 2';
 browserList.appendChild(newItem1);
+browserList.appendChild(newItem2);
 
-let myButton = document.getElementById("myButtonId");
-myButton.addEventListener('click',function () {
-    addListItemMessage("browser","klikol si na button")
+let myButton1 = document.getElementById("myButtonId1");
+let myButton2 = document.getElementById("myButtonId2");
+myButton1.addEventListener('click',function () {
+    addListItemMessage("browser","klikol si na button 1")
+});
+myButton2.addEventListener("click", function () {
+    addListItemMessage("browser", "klikol si na button 2")
 });
 
 document.body.addEventListener('keyup', function () {
