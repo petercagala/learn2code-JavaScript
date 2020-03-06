ddac7959efcd9466544d24f52dd3ed5f6b9139ea
Peter Čagala <petercagala@gmail.com>
2020-03-06 11:54:55 +0100

    [15-2] Praca s lodash.com, teda praca s poliami

--- src/main/resources/templates/js/script-guy.js
+++ src/main/resources/templates/js/script-guy.js
@@ -22,14 +22,19 @@ imagesElements.forEach(function (imageElement) {
     });
 
     // mouse enters, the other guy hurts
-    imageElement.addEventListener('mouseover', function () {
+    imageElement.addEventListener('mouseover', function mouseoverEvent() {
+        // https://lodash.com/docs/4.17.15#without
+        // _.without(array, [values])
+        // Creates an array excluding all given values using SameValueZero for equality comparisons.
+        // array (Array): The array to inspect.
+        // [values] (...*): The values to exclude.
         var otherImage = _.without(imagesElements, this)[0];
 
         otherImage.classList.add('desaturate');
     });
 
     // mouse leaves
-    imageElement.addEventListener('mouseout', function () {
+    imageElement.addEventListener('mouseout', function mouseoutEvent() {
         var otherImage = _.without(imagesElements, this)[0];
 
         otherImage.classList.remove('desaturate');
