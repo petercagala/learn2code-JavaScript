541fdf0eb0a17faf5253891b874bf5673ce825f2
Peter Čagala <petercagala@gmail.com>
2020-03-10 09:23:36 +0100

    [30-1] Exercise 6

---
+++ src/main/resources/templates/js/exercise6/script-exercise6.js
@@ -0,0 +1,43 @@
+var dudesList = DudesResource.dudes;
+
+var rowsElement = document.getElementById("rows");
+
+var numberOdDudes = 0;
+var numberOfComments = 0;
+
+dudesList.forEach(function (dude) {
+    var articleElement = document.createElement("article");
+
+    var aElement = document.createElement("a");
+    aElement.textContent = dude.who;
+    aElement.href = "dude/" + dude.who;
+
+    var h4Element = document.createElement("h4");
+    h4Element.appendChild(aElement);
+
+    var pElement = document.createElement("p");
+    pElement.textContent = dude.wat;
+
+    var footerElement = document.createElement("footer");
+    footerElement.textContent = dude.comments;
+    footerElement.className = "comments";
+
+    articleElement.appendChild(h4Element);
+    articleElement.appendChild(pElement);
+    articleElement.appendChild(footerElement);
+
+    rowsElement.appendChild(articleElement);
+});
+
+rowsElement.querySelectorAll("article").forEach(function (article) {
+    numberOdDudes = numberOdDudes +1;
+
+    let commentNumber = +article.getElementsByTagName("footer")[0].textContent;
+    numberOfComments = numberOfComments + commentNumber;
+});
+
+let numberOfDudesElement = document.getElementById("numberOfDudes");
+let numberOfCommentsElement = document.getElementById("numberOfComments");
+
+numberOfDudesElement.textContent = numberOdDudes;
+numberOfCommentsElement.textContent = numberOfComments;
---
+++ src/main/resources/templates/js/exercise6/script-exercise6.myModule.js
@@ -0,0 +1,33 @@
+var DudesResource = (
+    function () {
+        var dudes = [
+            {
+                "who": "Jake the Dog",
+                "wat": "Finn's best friend is a wise, old dog with a big heart. Jake has the magical ability to stretch and grow. When evil's not running amok, he plays viola with Lady Rainicorn.",
+                "comments": 23
+            },
+
+            {
+                "who": "Ice King",
+                "wat": "Armed with a magic crown and an icy heart, the Ice King has only one goal: to secure a wife by any means necessary.",
+                "comments": 10
+            },
+
+            {
+                "who": "Princess Bubblegum",
+                "wat": "As a millionaire nerd enthusiast, Princess Bubblegum immerses herself in every branch of geekdom from rocket science to turtle farming.",
+                "comments": 44
+            },
+
+            {
+                "who": "Marcy the Vampire",
+                "wat": "Marceline is a wild rocker girl. Centuries of wandering the Land of Ooo have made her a fearless daredevil.",
+                "comments": 10
+            }
+        ];
+        return {
+            dudes: dudes
+        };
+        // return dudes;
+    } ()
+);
