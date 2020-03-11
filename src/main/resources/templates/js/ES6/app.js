08e0e6755aecebefa12cb0679f3be36df08cf129
Peter Čagala <petercagala@gmail.com>
2020-03-11 07:49:02 +0100

    [47-1] ES6: use Arrow function

--- src/main/resources/templates/js/ES6/app.js
+++ src/main/resources/templates/js/ES6/app.js
@@ -1,1 +1,22 @@
-console.log("blablabla!!!")
+function multiMax(firstNumber, ...theRestNumbers) {
+    // Pozor ako return treba dat (b - a) teda cislo ani boolean b > a
+    let sortedTheRestNumbers = theRestNumbers.sort((a,b) => (b - a));
+
+    // let sortedTheRestNumbers = theRestNumbers.sort(
+    //     function sortMe(a, b) {
+    //         return b - a;
+    //     }
+    // );
+
+    let maxValue = sortedTheRestNumbers[0];
+
+    console.log("funkcia multiMax:");
+    console.log(sortedTheRestNumbers, maxValue);
+
+    return firstNumber * maxValue;
+}
+
+console.log(
+    "toto je prvy console log: ",
+    multiMax(5,2,1,2,16,3)
+);
