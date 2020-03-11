1d8edbd5d9c8f7c0a98004a671ef848226163487
Peter Čagala <petercagala@gmail.com>
2020-03-11 07:55:11 +0100

    [47-2] ES6: class, method, static method

--- src/main/resources/templates/js/ES6/app.js
+++ src/main/resources/templates/js/ES6/app.js
@@ -1,22 +1,25 @@
-function multiMax(firstNumber, ...theRestNumbers) {
-    // Pozor ako return treba dat (b - a) teda cislo ani boolean b > a
-    let sortedTheRestNumbers = theRestNumbers.sort((a,b) => (b - a));
+class SexyNumbers {
 
-    // let sortedTheRestNumbers = theRestNumbers.sort(
-    //     function sortMe(a, b) {
-    //         return b - a;
-    //     }
-    // );
+    // Moze to byt kludne aj static
+    multiMax(firstNumber, ...theRestNumbers) {
+        // Pozor ako return treba dat (b - a) teda cislo ani boolean b > a
+        let sortedTheRestNumbers = theRestNumbers.sort((a,b) => (b - a));
 
-    let maxValue = sortedTheRestNumbers[0];
+        // let sortedTheRestNumbers = theRestNumbers.sort(
+        //     function sortMe(a, b) {
+        //         return b - a;
+        //     }
+        // );
 
-    console.log("funkcia multiMax:");
-    console.log(sortedTheRestNumbers, maxValue);
+        let maxValue = sortedTheRestNumbers[0];
 
-    return firstNumber * maxValue;
+        console.log("funkcia multiMax:");
+        console.log(sortedTheRestNumbers, maxValue);
+
+        return firstNumber * maxValue;
+    }
 }
 
 console.log(
-    "toto je prvy console log: ",
-    multiMax(5,2,1,2,16,3)
+    new SexyNumbers().multiMax(5,2,1,2,16,3)
 );
