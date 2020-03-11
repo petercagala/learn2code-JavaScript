f6324d0160e37b70ebee064411ce1e90079bb4dc
Peter Čagala <petercagala@gmail.com>
2020-03-11 08:40:54 +0100

    [48-1] ES6: default params, Class, methods

--- src/main/resources/templates/js/ES6/app.js
+++ src/main/resources/templates/js/ES6/app.js
@@ -1,9 +1,16 @@
+'use strict';
+
+const TAX = 0.2;
+
+/**
+ *
+ */
 class SexyNumbers {
 
     // Moze to byt kludne aj static
-    multiMax(firstNumber, ...theRestNumbers) {
+    static multiMax(firstNumber, ...theRestNumbers) {
         // Pozor ako return treba dat (b - a) teda cislo ani boolean b > a
-        let sortedTheRestNumbers = theRestNumbers.sort((a,b) => (b - a));
+        let sortedTheRestNumbers = theRestNumbers.sort((a, b) => (b - a));
 
         // let sortedTheRestNumbers = theRestNumbers.sort(
         //     function sortMe(a, b) {
@@ -20,6 +27,35 @@ class SexyNumbers {
     }
 }
 
+/**
+ *
+ */
+class SexyNumbersExtended extends SexyNumbers {
+
+    /**
+     * Ja mozem priamo parametru prednastavit percent = 10, ak tomuto parametru nic neposlem
+     *
+     * Kludne mozem hodnotu nezadaneho parametra dostat aj volanim inej funkcie, alebo metody
+     * @param price
+     * @param percent
+     * @returns {string}
+     */
+    static discount(price, percent = 10, tax = getTax()) {
+        let newPrice = price - price * percent * 0.01;
+        newPrice = newPrice - newPrice * tax;
+        return newPrice.toFixed(2);
+    }
+
+}
+
+function getTax() {
+    return TAX;
+}
+
 console.log(
-    new SexyNumbers().multiMax(5,2,1,2,16,3)
+    SexyNumbers.multiMax(5, 2, 1, 2, 16, 3)
 );
+
+console.log(
+    SexyNumbersExtended.discount(100, 20)
+)
