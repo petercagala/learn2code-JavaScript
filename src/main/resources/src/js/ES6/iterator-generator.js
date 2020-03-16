978a3894ab6f29a511e0ac42a80f4687b8ee362d
Peter Čagala <petercagala@gmail.com>
2020-03-16 16:43:22 +0100

    [74-1] Generator functions, generovanie id, vyuzitie, ze pri volani next() sa to zastavi na yield-e

--- src/main/resources/src/js/ES6/iterator-generator.js
+++ src/main/resources/src/js/ES6/iterator-generator.js
@@ -14,3 +14,19 @@ for (let variable of iterableVariable) {
     console.log(variable);
 }
 
+/**
+ * Hviezdicka je krasny generator
+ * @returns {Generator<number, void, ?>}
+ */
+function * generatorId() {
+    let id = 0;
+    while(true) {
+        // pri generatoroch sa nemusis bat, pretorze akonahle narazi na prvy yield, zastavi svoch chod a caka na dalsi next()
+        if (id == 5) {
+            break;
+        } else {
+            yield ++id; // tuna to vzdy zastavi
+        }
+    }
+
+}
