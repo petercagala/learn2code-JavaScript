c7c94665074f4c90ae2094b301e43ff96c6cc3fa
Peter Čagala <petercagala@gmail.com>
2020-03-17 11:43:47 +0100

    [76-1] Promisses , AJAX, zaklady posielania requestov a osetrenia promise cez then() a catch()

--- src/main/resources/src/js/ES6/script-promisses.js
+++ src/main/resources/src/js/ES6/script-promisses.js
@@ -1,11 +1,17 @@
-var promise = get("https://itunes.apple.com/search?term=kolowrat");
+// REQ https://itunes.apple.com/search?term=kolowrat je nejaky dodzubany
+// var promise = get("https://itunes.apple.com/search?term=kolowrat");
+var promise = get("https://jsonplaceholder.typicode.com/comments");
 
+// promise.then(
+//     result => console.log(JSON.parse(result))
+// ).catch(
+//     error => console.error(error, "Oh no, it is my false")
+// );
+
+// Dokonca moze byt error aj priamo v then()
 promise.then(
-    result => console.log(JSON.parse(result))
-);
-
-promise.catch(
-    // error => console.error(error, "Oh no, it is my false")
+    result => console.log(JSON.parse(result)),
+    error => console.error(error, "Oh no, it is my false")
 );
 
 /**
